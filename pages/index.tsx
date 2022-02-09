b51c48906111d059f4bd6ca9482709a01f887fd7
Menghao Yu <menghaoyu2002@gmail.com>
2022-02-09 02:06:40 -0500

    added image fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import axios from '../config/axios';
 import type { NextPage } from 'next';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import IImage from '../types/IImages';
+import { ImageCarosel } from '../components/ImageCarosel';
 
 const Home: NextPage = () => {
-    return <h1> Hello</h1>;
+    const [images, setImages] = useState<IImage[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    useEffect(() => {
+        const fetchImages = async () => {
+            const res = await axios.get('/images?limit=20&page=1');
+            setImages(res.data);
+            setLoading(false);
+        };
+        fetchImages().catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <div>
+            <h1> Welcome to the Vault</h1>
+            {loading ? <p>Loading</p> : <ImageCarosel images={images} />}
+        </div>
+    );
 };
 
 export default Home;
